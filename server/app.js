4d487fea327a763c9d0a8c06cb6244fae6452309
Anatol Manoila  <anatol.manoila@tellence.com>
2017-01-14 14:14:19 +0200

    add mongo connection and list sports values

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 "use strict";
 let express = require('express');
 let app = express();
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
 
 app.use(express.static(__dirname + '/../client'));
 
-
 app.get('/sports', (req, res) => {
-  res.json(['AquaDance', 'Abs'])
+  let sports = mongoUtil.sports();
+  sports.find().toArray((err, docs) => {
+    console.log(JSON.stringify(docs));
+    let sportNames = docs.map((sport) => sport.name);
+    res.json(sportNames);
+  });
 });
 
 app.listen(5000, () => console.log('listening on http://localhost:5000'));
---
+++ server/mongoUtil.js
@@ -0,0 +1,18 @@
+'use strict';
+let mongo = require('mongodb');
+let client = mongo.MongoClient;
+let _db;
+
+module.exports = {
+  connect() {
+    client.connect('mongodb://localhost:27017/myOlympics-dev', (err, db) => {
+      if(err){console.log('Error connecting to mongo - check Mongo connection');process.exit(1);}
+
+      _db = db;
+      console.log('Connected to Mongo');
+    });
+  },
+  sports() {
+    return _db.collection('sports');
+  }
+};
