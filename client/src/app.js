01256683c30f24f6ee5fbc3f829c64c4f565bb9d
Anatol Manoila  <anatol.manoila@tellence.com>
2017-01-16 13:13:44 +0200

    chore: simulate angular sport data service with q

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,49 @@
 import angular from 'angular'
-angular.module('myOlympics', [])
-.controller('sportsController', function($http) {
-  $http.get('/sports').then((response) => {
-    this.sports = response.data;
-  });
+import 'angular-ui-router'
+angular.module('myOlympics', ['ui.router'])
+
+.config(($stateProvider, $urlRouterProvider) => {
+  $urlRouterProvider.otherwise('/sports');
+
+  $stateProvider
+    .state('sports', {
+      url: '/sports',
+      templateUrl: 'modules/sports/sports-nav.html',
+      resolve: {
+        sportsService: function($http) {
+          return $http.get('/sports');
+        }
+      },
+      controller: function(sportsService) {
+        this.sports = sportsService.data;
+      },
+      controllerAs: 'sportsCtrl'
+    }).
+    state('sports.medals', {
+      url: '/:sportName',
+      templateUrl: 'modules/sports/sports-medals.html',
+      resolve: {
+        sportService: function($q) {
+          return $q((resolve, reject) => {
+            let sport = {
+              "name": "Mountain Biking",
+              "goldMedals": [{
+                "division": "Men's Sprint",
+                "country": "RO",
+                "year": 2016
+              }, {
+                "division": "Men\'s line",
+                "country": "MD",
+                "year": 2016
+            }]
+          };
+          resolve( { data: sport } );
+          });
+        }
+      },
+      controller: function(sportService) {
+        this.sport = sportService.data;
+      },
+      controllerAs: 'sportCtrl'
+    });
 });
--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -5,7 +5,7 @@ let _db;
 
 module.exports = {
   connect() {
-    client.connect('mongodb://localhost:27017/myOlympics-dev', (err, db) => {
+    client.connect('mongodb://localhost:27017/olympics-dev', (err, db) => {
       if(err){console.log('Error connecting to mongo - check Mongo connection');process.exit(1);}
 
       _db = db;
