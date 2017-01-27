7ed05f7a9e3c0b1477d93a66f616245b3642e3bd
Anatol Manoila  <anatol.manoila@tellence.com>
2017-01-27 15:15:58 +0200

    add POST medal endpoint

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@
 let express = require('express');
 let app = express();
 let mongoUtil = require('./mongoUtil');
+let bodyParser = require('body-parser');
+let jsonParser = bodyParser.json();
 mongoUtil.connect();
 
 app.use(express.static(__dirname + '/../client'));
@@ -30,6 +32,16 @@ app.get('/sports/:name', (req, res) => {
     //console.log('Sport doc: ', doc);
     res.json(doc);
   });
+});
+
+app.post('/sports/:name/medals', jsonParser, (req, res) => {
+  let sportName = req.params.name;
+  let newMedal  = req.body.medal;
+
+  console.log("Sport name: ", sportName );
+  console.log("Medal: ", newMedal);
+
+  res.sendStatus(201);
 });
 
 app.listen(5000, () => console.log('listening on http://localhost:5000'));
